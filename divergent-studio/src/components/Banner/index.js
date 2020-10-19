108d40c1227c5967ce1356765f2bddb3bf0042ad
ritamedina27 <ritamedina27@gmail.com>
2020-10-19 21:24:25 +0100

    finished title and button

--- divergent-studio/src/components/Banner/index.js
+++ divergent-studio/src/components/Banner/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./styles.css";
+import whatsappIcon from "../../assets/whatsapp.png";
 
 const Banner = (props) => {
   return (
-    <div className="container">
+    <>
       <div className="header">
-        <button>Let's Talk 👋</button>
+        <button>
+          <p>Let's Talk</p>
+          <img src={whatsappIcon} alt="icone" width={27} height="27" />
+        </button>
       </div>
-      <div className="banner-container">
+      <div className="container">
         <h1>
           <strong>Go divergent</strong> - for the ones looking to combine
           beautiful and honest design with interactive and professional coding.
         </h1>
       </div>
-    </div>
+    </>
   );
 };
 
