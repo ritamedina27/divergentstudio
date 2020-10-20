5722597eade047e6b5ccd0938ec96e5e418ba347
ritamedina27 <ritamedina27@gmail.com>
2020-10-20 15:22:05 +0100

    just started

---
+++ divergent-studio/src/components/OurProcess/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const OurProcess = (props) => {
+  return (
+    <div className="container">
+      <h1>Our Process</h1>
+    </div>
+  );
+};
+
+export default OurProcess;
