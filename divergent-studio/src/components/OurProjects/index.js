45fc2f19d920aa274c78d876e921a92a625e0e2c
ritamedina27 <ritamedina27@gmail.com>
2020-11-01 18:09:19 +0000

    titulo e subtitulo

--- divergent-studio/src/components/OurProjects/index.js
+++ divergent-studio/src/components/OurProjects/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import './styles.css';
-import getProjects from '../../api/getProject';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import getProjects from "../../api/getProject";
 function OurProjects() {
   const [projects, setProjects] = useState();
 
@@ -15,14 +15,27 @@ function OurProjects() {
 
   return (
     <div className="ourProjects">
+      <div class Name="container">
+        <div>
+          <h1>
+            Our<p>Portfolio</p>
+          </h1>
+        </div>
+        <h2>
+          We are an Iberian dupla, working from Lisbon. Sometimes from
+          Barcelona.
+        </h2>
+      </div>
       {projects &&
         projects.data?.records.map((item) => (
-          <div className="project">
-            <h1>{item.fields.Name}</h1>
-            {item.fields.Image && (
-              <img src={item.fields.Image[0].url} height="200px"></img>
-            )}
-            <p>{item.fields.Descritpion}</p>
+          <div className="container">
+            <div className="project">
+              <h1>{item.fields.Name}</h1>
+              {item.fields.Image && (
+                <img src={item.fields.Image[0].url} height="200px"></img>
+              )}
+              <p>{item.fields.Descritpion}</p>
+            </div>
           </div>
         ))}
     </div>
